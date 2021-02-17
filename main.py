9fbbf6bbcc48cccb50a8768ce8c4b65c2c97f764
iseelise <39001717+elisepitel@users.noreply.github.com>
2021-02-17 06:26:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ franklin.penup()
 
 ### Once list is created within the console, you can save the output with a list of tuple ↓ and comment the code above ↑ 
 colors_list = [(234, 62, 100), (233, 149, 88), (248, 218, 97), (2, 180, 212), (162, 53, 97), (236, 121, 146), (1, 132, 167), (236, 79, 66), (111, 190, 153), (2, 155, 100), (86, 191, 211)]
-### You might white color's tuples within your list, do not hesitate to test your colors with a Colors RGB calculator (exmpl : https://www.w3schools.com/colors/colors_rgb.asp)
+### You might white color's tuples within your list, do not hesitate to test your colors with a Colors RGB calculator 
+### (exmpl : https://www.w3schools.com/colors/colors_rgb.asp)
 
 
 y_position = -200
